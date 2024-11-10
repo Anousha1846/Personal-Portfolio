4fa1dda2077b2e1f17a2a6455b89d31e0ddb7c31
Anousha1846 <anoushaasadullah18@gmail.com>
2024-11-10 14:07:22 -0500

    Your commit message

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
+import { output } from "framer-motion/client";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 module.exports = {
   basePath: '/My-Portfolio-Website',  // Replace with your GitHub repository name
   assetPrefix: '/My-Portfolio-Website/',  // Same as above
+  output: 'export'
 }
   export default nextConfig;
 
