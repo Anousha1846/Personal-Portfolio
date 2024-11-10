f1588dce7b8ae85a3ad2b28cfa3f6cd82ec35dc6
Anousha1846 <anoushaasadullah18@gmail.com>
2024-11-10 13:28:33 -0500

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,5 @@
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  basePath: '/<My-Portfolio>', // Replace <repo-name> with your GitHub repository name
-  images: {
-    unoptimized: true, // Required for GitHub Pages
-  },
-};
-
-module.exports = nextConfig;
+module.exports = {
+  basePath: '/My-Portfolio',  // Replace with your GitHub repository name
+  assetPrefix: '/My-Portfolio/',  // Same as above
+}
